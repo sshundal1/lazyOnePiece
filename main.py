1020838c460472a24891bedb0081e963be0df75d
Shawn Hundal <sshundal@ucdavis.edu>
2022-07-09 11:02:44 -0700

    My first add of the import

---
+++ main.py
@@ -0,0 +1,15 @@
+import animdl
+from animdl.__main__ import __animdl_cli__
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    __animdl_cli__("--help")
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
